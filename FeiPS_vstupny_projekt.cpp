c904a11e426913c3b0f1c3cb87a51511081aaf52
Xlucny <xlucny@stuba.sk>
2022-02-21 22:04:37 +0100

    first tests

--- FeiPS_vstupny_projekt.cpp
+++ FeiPS_vstupny_projekt.cpp
@@ -55,7 +55,7 @@ int main(int argc, char* argv[]) {
 
     RandomNumberGenerator rng;
 
-    for (int i = 0; i < BUFFER_SIZE;) {
+    for (int i = 0; i < BUFFER_SIZE;i++) {
         buffer[i] = rng.GenerateRandomNumber();
     }
 
